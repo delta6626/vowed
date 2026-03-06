5ebee2438c34b72ec68bb6f44d41a9fe978ffaf9
Delta6626 <hassan911ee@gmail.com>
2026-03-07 01:21:49 +0400

    add daisyui + themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <h1>
+        You <span>vowed</span> it.
+      </h1>
+      <br />
+      <h1>Now prove it.</h1>
+    </div>
+  );
 }
