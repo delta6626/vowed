8b95f8fec26b53e2d2e331c3b9c15191dca66cd4
Delta6626 <hassan911ee@gmail.com>
2026-03-09 01:15:27 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,13 +19,13 @@ export default function Home() {
         </div>
 
         <div className="font-display flex flex-col gap-8 mt-16">
-          <h1 className="text-7xl leading-[1.1]">
+          <h1 className="text-6xl leading-[1.1]">
             You <span className="text-primary italic">vowed</span> it.
             <br />
             Now prove it.
           </h1>
 
-          <h3 className="text-3xl italic text-accent">
+          <h3 className="text-2xl italic text-accent">
             Set a deadline. Share the link. <br />
             The world is watching.
           </h3>
