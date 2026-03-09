4308f83dceffadcbaa0012f3216183dd46229d2f
Delta6626 <hassan911ee@gmail.com>
2026-03-09 19:27:56 +0400

    change layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,42 @@
 import Navbar from "@/components/navigation/Navbar";
 import HeroCTA from "@/components/landing/HeroCTA";
 import Pill from "@/components/generic/Pill";
-import { Dot } from "lucide-react";
+import Footer from "@/components/navigation/Footer";
 
 export default function Home() {
   return (
-    <div className="">
-      <Navbar />
+    <div className="flex flex-col w-screen h-screen">
+      <div className="flex flex-col flex-1">
+        <Navbar />
 
-      <div className="w-full text-center paddingContainer font-body">
-        <div className="w-full flex justify-center mt-16">
-          <Pill
-            variant="generic"
-            icon={<div className="w-2 h-2 rounded-full bg-primary"></div>}
-            text={"Public commitment platform"}
-            className="uppercase"
-          />
-        </div>
+        <div className="w-full text-center paddingContainer font-body">
+          <div className="w-full flex justify-center mt-16">
+            <Pill
+              variant="generic"
+              icon={<div className="w-2 h-2 rounded-full bg-primary"></div>}
+              text={"Public commitment platform"}
+              className="uppercase"
+            />
+          </div>
 
-        <div className="font-display flex flex-col gap-8 mt-16">
-          <h1 className="text-6xl leading-[1.1]">
-            You <span className="text-primary italic">vowed</span> it.
-            <br />
-            Now prove it.
-          </h1>
+          <div className="font-display flex flex-col gap-8 mt-16">
+            <h1 className="text-6xl leading-[1.1]">
+              You <span className="text-primary italic">vowed</span> it.
+              <br />
+              Now prove it.
+            </h1>
 
-          <h3 className="text-2xl italic text-accent">
-            Set a deadline. Share the link. <br />
-            The world is watching.
-          </h3>
+            <h3 className="text-2xl italic text-accent">
+              Set a deadline. Share the link. <br />
+              The world is watching.
+            </h3>
 
-          <HeroCTA />
+            <HeroCTA />
+          </div>
         </div>
       </div>
+
+      <Footer />
     </div>
   );
 }
