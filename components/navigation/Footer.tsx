a0fbdb307dcd8852f7da55c22a9780d2deff5f74
Delta6626 <hassan911ee@gmail.com>
2026-03-09 19:44:20 +0400

    Update Footer.tsx

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -1,5 +1,16 @@
+import Logo from "./Logo";
+
 export default function Footer() {
   return (
-    <footer className="w-full paddingContainer py-4 border-t border-base-300"></footer>
+    <footer className="font-body w-full paddingContainer py-4 border-t border-base-300">
+      <div className="">
+        <Logo className="text-xl" />
+        <p className="max-w-40 mt-2 text-accent text-sm">
+          The internet never forgets. Neither do we.
+        </p>
+      </div>
+
+      <div></div>
+    </footer>
   );
 }
