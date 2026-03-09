bfea97b61b2ec7ab19d3c4e38378fdc6e990db18
Delta6626 <hassan911ee@gmail.com>
2026-03-09 19:56:48 +0400

    add links

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import Logo from "./Logo";
+import { Dot } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="w-full paddingContainer py-4 border-t border-base-300">
+    <footer className="flex items-center justify-between w-full paddingContainer py-4 border-t border-base-300">
       <div className="">
         <Logo className="text-xl" />
         <p className="max-w-40 mt-2 text-accent text-sm">
@@ -10,7 +12,23 @@ export default function Footer() {
         </p>
       </div>
 
-      <div></div>
+      <div className="flex gap-4">
+        <Link className="text-sm text-accent" href={"/privacy-policy"}>
+          Privacy
+        </Link>
+
+        <Dot className="text-accent" size={20} />
+
+        <Link className="text-sm text-accent" href={"/terms-of-service"}>
+          Terms
+        </Link>
+
+        <Dot className="text-accent" size={20} />
+
+        <Link className="text-sm text-accent" href={"/contact"}>
+          Contact
+        </Link>
+      </div>
     </footer>
   );
 }
