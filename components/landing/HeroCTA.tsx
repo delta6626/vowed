0775cd802994188a21f294176a675f5051a431a8
Delta6626 <hassan911ee@gmail.com>
2026-03-10 23:20:04 +0400

    Update HeroCTA.tsx

--- components/landing/HeroCTA.tsx
+++ components/landing/HeroCTA.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 export default function HeroCTA() {
   return (
     <div className="w-full flex justify-center gap-4">
-      <button className="btn btn-primary">Make your first vow</button>
+      <Link href={"/sign-up"} className="btn btn-primary">
+        Make your first vow <ArrowRight size={20} />
+      </Link>
       <button className="btn">See how it works</button>
     </div>
   );
