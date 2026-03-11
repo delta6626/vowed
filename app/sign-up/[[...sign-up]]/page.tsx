c56c263504c04cbe7737c6c67b007484a5eb5a4b
Delta6626 <hassan911ee@gmail.com>
2026-03-11 23:24:56 +0400

    Update page.tsx

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,14 @@
+import SideSection from "@/components/auth/SideSection";
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-  return <SignUp />;
+  return (
+    <div className="w-full flex">
+      <SideSection />
+
+      <div className="w-full flex items-center bg-base-200 justify-center">
+        <SignUp />
+      </div>
+    </div>
+  );
 }
