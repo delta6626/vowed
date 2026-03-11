ea39e4bab2a2f2322024555f7fee5e1702675a0a
Delta6626 <hassan911ee@gmail.com>
2026-03-11 23:25:12 +0400

    Create SideSectionContent.tsx

---
+++ components/auth/SideSectionContent.tsx
@@ -0,0 +1,14 @@
+export default function SideSectionContent() {
+  return (
+    <div className="text-4xl leading-[1.1] font-display">
+      <h1>
+        Make it <span className="text-primary italic">public.</span>
+      </h1>
+      <h1>
+        Make it <span className="text-primary italic">count.</span>
+      </h1>
+
+      <div></div>
+    </div>
+  );
+}
