10f1efb5941879c368ed7bce05031200eff53844
Delta6626 <hassan911ee@gmail.com>
2026-03-11 23:25:14 +0400

    Create SideSection.tsx

---
+++ components/auth/SideSection.tsx
@@ -0,0 +1,20 @@
+import { Dot } from "lucide-react";
+import Logo from "../navigation/Logo";
+import SideSectionContent from "./SideSectionContent";
+
+export default function SideSection() {
+  return (
+    <div className="flex flex-col justify-between paddingContainer py-4 w-full h-screen bg-base-300">
+      <Logo className="text-3xl" />
+      <SideSectionContent />
+
+      <div className="flex items-center">
+        <Logo className="text-lg" />
+        <Dot className="text-accent" size={20} />
+        <p className="text-accent">
+          Public commitments - permanently on record.
+        </p>
+      </div>
+    </div>
+  );
+}
