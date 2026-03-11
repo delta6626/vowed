8f2a425285bd1390b54678319eb4d029cc4cb5e9
Delta6626 <hassan911ee@gmail.com>
2026-03-11 21:10:34 +0400

    wrap app with clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import { Playfair_Display, DM_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
@@ -22,7 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${dmSans.variable}`}>
       <body className="font-body">
-        <ThemeProvider>{children}</ThemeProvider>
+        <ClerkProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
