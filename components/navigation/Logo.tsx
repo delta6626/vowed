5e421a7b237c9e8b7ca175691255f0cf894a21a0
Delta6626 <hassan911ee@gmail.com>
2026-03-11 23:25:00 +0400

    Update Logo.tsx

--- components/navigation/Logo.tsx
+++ components/navigation/Logo.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export interface LogoProps {
   className?: string;
 }
 
 export default function Logo({ className }: LogoProps) {
   return (
-    <h1 className={`font-display ${className}`}>
+    <Link href={"/"} className={`font-display ${className}`}>
       <span className="text-primary">Vowed</span>.cc
-    </h1>
+    </Link>
   );
 }
