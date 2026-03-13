b6161a68176e0064cde89f35111ffbfdee36804f
Delta6626 <hassan911ee@gmail.com>
2026-03-13 16:53:40 +0400

    integrate dashboard statbox

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import DashboardStatBox from "@/components/generic/DashboardStatBox";
 import Navbar from "@/components/navigation/Navbar";
 import { getGreeting } from "@/utils/functions/getGreeting";
 import { currentUser } from "@clerk/nextjs/server";
@@ -8,15 +9,23 @@ export default async function Dashboard() {
   return (
     <div className="w-screen h-screen">
       <Navbar />
-      <div className="doublePaddingContainer mt-16">
-        <h1 className="font-display text-4xl">
-          {getGreeting() + ", "}
-          <span className="text-primary italic">{user.firstName}.</span>
-        </h1>
+      <div className="doublePaddingContainer">
+        <div className="mt-16">
+          <h1 className="font-display text-4xl">
+            {getGreeting() + ", "}
+            <span className="text-primary italic">{user.firstName}.</span>
+          </h1>
 
-        <p className="font-body text-accent text-xl mt-2">
-          Here's where your vows stand today.
-        </p>
+          <p className="font-body text-accent text-xl mt-2">
+            Here's where your vows stand today.
+          </p>
+        </div>
+
+        <div className="flex gap-4 mt-8">
+          <DashboardStatBox statBoxType="active" mainText="7" />
+          <DashboardStatBox statBoxType="fulfillment" mainText="92%" />
+          <DashboardStatBox statBoxType="total" mainText="17" />
+        </div>
       </div>
     </div>
   );
