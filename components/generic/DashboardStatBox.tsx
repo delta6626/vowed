41f5aff818d333505af139f6f8a2e98a95dac105
Delta6626 <hassan911ee@gmail.com>
2026-03-13 16:53:22 +0400

    create dashboard stat box component to hold stats

--- components/generic/DashboardStatBox.tsx
+++ components/generic/DashboardStatBox.tsx
@@ -1,4 +1,5 @@
 import { StatBoxType } from "@/types/StatBoxType";
+import { getFormattedStatBoxName } from "@/utils/functions/getFormattedStatBoxName";
 
 export interface DashboardStatBoxProps {
   statBoxType: StatBoxType;
@@ -9,5 +10,28 @@ export default function DashboardStatBox({
   statBoxType,
   mainText,
 }: DashboardStatBoxProps) {
-  return <div></div>;
+  let mainTextClassName;
+
+  switch (statBoxType) {
+    case "total":
+      mainTextClassName = "";
+      break;
+    case "fulfillment":
+      mainTextClassName = "text-success";
+      break;
+    case "active":
+      mainTextClassName = "text-primary";
+      break;
+  }
+
+  return (
+    <div className="w-60 min-h-28 p-4 rounded-2xl border border-base-300 bg-base-200 flex flex-col items-center justify-between">
+      <div className="flex flex-1 items-center justify-center">
+        <h1 className={`text-4xl font-display ${mainTextClassName}`}>
+          {mainText}
+        </h1>
+      </div>
+      <p className="text-accent">{getFormattedStatBoxName(statBoxType)}</p>
+    </div>
+  );
 }
