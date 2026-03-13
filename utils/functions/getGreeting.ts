fea4a770668de29177e9e705200920006540eabc
Delta6626 <hassan911ee@gmail.com>
2026-03-13 15:12:36 +0400

    create utility function

---
+++ utils/functions/getGreeting.ts
@@ -0,0 +1,7 @@
+export const getGreeting = () => {
+  const hour = new Date().getHours();
+
+  if (hour < 12) return "Good morning";
+  if (hour < 18) return "Good afternoon";
+  return "Good evening";
+};
