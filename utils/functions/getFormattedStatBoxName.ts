5970e98e5802262bcf778045b5c5bd4da3eb4a18
Delta6626 <hassan911ee@gmail.com>
2026-03-13 16:51:43 +0400

    utility func to get formatted stat box name

---
+++ utils/functions/getFormattedStatBoxName.ts
@@ -0,0 +1,12 @@
+import type { StatBoxType } from "@/types/StatBoxType";
+
+export const getFormattedStatBoxName = (statBoxType: StatBoxType) => {
+  switch (statBoxType) {
+    case "total":
+      return "Total vows";
+    case "fulfillment":
+      return "Fulfillment rate";
+    case "active":
+      return "Active vows";
+  }
+};
