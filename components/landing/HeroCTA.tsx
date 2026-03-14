e11169741f7415ecef5b6b596eb5c8dde1dc2677
Delta6626 <hassan911ee@gmail.com>
2026-03-14 17:21:58 +0400

    refactor

--- components/landing/HeroCTA.tsx
+++ components/landing/HeroCTA.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { SignUpButton } from "@clerk/nextjs";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 export default function HeroCTA() {
   return (
     <div className="w-full flex justify-center gap-4">
-      <Link href={"/sign-up"} className="btn btn-primary">
-        Make your first vow <ArrowRight size={20} />
-      </Link>
+      <SignUpButton>
+        <button className="btn btn-primary">
+          Make your first vow <ArrowRight size={20} />
+        </button>
+      </SignUpButton>
       <button className="btn">See how it works</button>
     </div>
   );
