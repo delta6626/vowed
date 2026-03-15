8f6507b23af9f1a89b0bc44ded4e62f481d7a21f
Delta6626 <hassan911ee@gmail.com>
2026-03-15 22:40:31 +0400

    update type

--- types/User.ts
+++ types/User.ts
@@ -3,6 +3,7 @@ import { Plan } from "./Plan";
 export interface User {
   clerkId: string;
   displayName: string;
+  emailAddress: string;
   avatarURL: string;
   plan: Plan;
   vowsCreated: number;
