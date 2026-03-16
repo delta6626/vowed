395ff94734589c1b10b5f381fd099e9341f477ef
Delta6626 <hassan911ee@gmail.com>
2026-03-16 22:41:02 +0400

    update hero CTA

--- components/landing/HeroCTA.tsx
+++ components/landing/HeroCTA.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import { SignUpButton } from "@clerk/nextjs";
+import { Show, SignUpButton } from "@clerk/nextjs";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 export default function HeroCTA() {
   return (
     <div className="w-full flex justify-center gap-4">
-      <SignUpButton>
-        <button className="btn btn-primary">
-          Make your first vow <ArrowRight size={20} />
-        </button>
-      </SignUpButton>
+      <Show when={"signed-out"}>
+        <SignUpButton>
+          <button className="btn btn-primary">
+            Make your first vow <ArrowRight size={20} />
+          </button>
+        </SignUpButton>
+      </Show>
+
+      <Show when={"signed-in"}>
+        <Link href={"/dashboard"} className="btn btn-primary">
+          Go to Dashboard
+        </Link>
+      </Show>
       <button className="btn">See how it works</button>
     </div>
   );
