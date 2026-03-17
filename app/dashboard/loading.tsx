bc19447dc3657957f3bdcdc4ec0eb73dfc08915d
Delta6626 <hassan911ee@gmail.com>
2026-03-17 19:46:48 +0400

    add skeleton

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,1 +1,12 @@
-export default function Loading() {}
+import Navbar from "@/components/navigation/Navbar";
+
+export default function Loading() {
+  return (
+    <>
+      <Navbar />
+      <div className="doublePaddingContainer py-16">
+        <div className="skeleton w-full h-20 rounded-2xl"></div>
+      </div>
+    </>
+  );
+}
