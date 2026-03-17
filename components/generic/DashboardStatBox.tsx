08edbf255e985a1c3ea52ca1a1e64efdf7e6e3fd
Delta6626 <hassan911ee@gmail.com>
2026-03-17 23:30:39 +0400

    update style

--- components/generic/DashboardStatBox.tsx
+++ components/generic/DashboardStatBox.tsx
@@ -17,17 +17,17 @@ export default function DashboardStatBox({
       mainTextClassName = "";
       break;
     case "fulfillment":
-      mainTextClassName = "text-success";
+      mainTextClassName = "";
       break;
     case "active":
-      mainTextClassName = "text-primary";
+      mainTextClassName = "";
       break;
   }
 
   return (
     <div className="w-60 min-h-28 p-4 rounded-2xl border border-base-300 bg-base-200 flex flex-col items-center justify-between">
       <div className="flex flex-1 items-center justify-center">
-        <h1 className={`text-4xl font-bold ${mainTextClassName}`}>
+        <h1 className={`text-4xl font-semibold ${mainTextClassName}`}>
           {mainText}
         </h1>
       </div>
