08fb49f0886726255f14a880c74f5d0cfd36ce5b
Delta6626 <hassan911ee@gmail.com>
2026-03-17 22:45:31 +0400

    fetch active vows

--- components/dashboard/ActiveVowsContainer.tsx
+++ components/dashboard/ActiveVowsContainer.tsx
@@ -1,5 +1,13 @@
+import { NEXT_TAGS } from "@/constants/NEXT_TAGS";
 import DashboardStatBox from "../generic/DashboardStatBox";
 
-export default function ActiveVowsContainer() {
-  return <DashboardStatBox statBoxType="active" mainText="" />;
+export default async function ActiveVowsContainer() {
+  const req = await fetch("api/user/vow-count", {
+    next: { tags: [NEXT_TAGS.DASHBOARD_STATS] },
+  });
+  const res = await req.json();
+
+  const activeVows = res.activeVowCount;
+
+  return <DashboardStatBox statBoxType="active" mainText={activeVows} />;
 }
