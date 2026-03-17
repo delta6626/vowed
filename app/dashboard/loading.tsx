e6ef5ba61591cb761b8e51bcebb92f1de6d80097
Delta6626 <hassan911ee@gmail.com>
2026-03-17 19:55:33 +0400

    add skeleton divs

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -6,6 +6,12 @@ export default function Loading() {
       <Navbar />
       <div className="doublePaddingContainer py-16">
         <div className="skeleton w-full h-20 rounded-2xl"></div>
+
+        <div className="flex gap-4 mt-8">
+          <div className="w-60 min-h-28 p-4 rounded-2xl skeleton"></div>
+          <div className="w-60 min-h-28 p-4 rounded-2xl skeleton"></div>
+          <div className="w-60 min-h-28 p-4 rounded-2xl skeleton"></div>
+        </div>
       </div>
     </>
   );
