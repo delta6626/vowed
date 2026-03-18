5e7a209923f400bc165a180567bc4e73e4ec0b95
Delta6626 <hassan911ee@gmail.com>
2026-03-18 23:42:37 +0400

    add title and subtitle

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -6,6 +6,20 @@ export default function Create() {
   return (
     <div className="w-screen h-screen">
       <Navbar />
+
+      <div className="doublePaddingContainer">
+        <div className="mt-16">
+          <h1 className="font-display text-4xl">
+            Make your <span className="text-primary">vow.</span>
+          </h1>
+          <p className="font-body text-accent text-xl mt-2">
+            Describe your vow. Set a deadline. It goes public the moment you
+            submit.
+            <br />
+            The world will be watching.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
