a5e9a2408dc773d8c51f3d35ebdd5372e896bb5b
Delta6626 <hassan911ee@gmail.com>
2026-03-18 23:34:18 +0400

    create new route

---
+++ app/create/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Navbar from "@/components/navigation/Navbar";
+
+export default function Create() {
+  return (
+    <div className="w-screen h-screen">
+      <Navbar />
+    </div>
+  );
+}
