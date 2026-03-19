4c7ee6cb82926cb0fa9c79e78535c1b7b4cb5b02
Delta6626 <hassan911ee@gmail.com>
2026-03-19 17:40:28 +0400

    Update Vow.ts

--- types/Vow.ts
+++ types/Vow.ts
@@ -1,11 +1,13 @@
 import { VowResolution } from "./VowResolution";
 import { VowStatus } from "./VowStatus";
+import { VowVisibility } from "./VowVisibility";
 
 export interface Vow {
   authorId: string;
   title: string;
   description: string | null;
   deadline: Date;
+  visibility: VowVisibility;
   status: VowStatus;
   resolution: VowResolution | null; // null until deadline
   viewCount: number;
