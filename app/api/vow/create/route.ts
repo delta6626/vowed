b702c4a632fff57e1f7be61e8ed116d37e78b68c
Delta6626 <hassan911ee@gmail.com>
2026-03-22 23:47:43 +0400

    add logic

--- app/api/vow/create/route.ts
+++ app/api/vow/create/route.ts
@@ -1,1 +1,29 @@
-export async function POST(req: Request) {}
+import { Vow } from "@/types/Vow";
+import { NextRequest, NextResponse } from "next/server";
+
+export type ClientVowDetails = Pick<
+  Vow,
+  "title" | "description" | "deadlineTimestampUTC" | "visibility"
+>;
+
+export async function POST(req: NextRequest) {
+  try {
+    const vowDetails = (await req.json()) as ClientVowDetails;
+
+    if (
+      !vowDetails.title ||
+      !vowDetails.deadlineTimestampUTC ||
+      !vowDetails.visibility
+    ) {
+      return NextResponse.json(
+        { error: "One or more vow attributes are missing." },
+        { status: 400 },
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "An error occured on the server." },
+      { status: 500 },
+    );
+  }
+}
