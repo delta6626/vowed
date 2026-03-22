ac14b3696989b55f0548b353542d969806ec8289
Delta6626 <hassan911ee@gmail.com>
2026-03-22 20:51:59 +0400

    create vow preview

---
+++ components/create/VowPreview.tsx
@@ -0,0 +1,7 @@
+import { Vow } from "@/types/Vow";
+
+export type VowPreviewProps = Pick<Vow, "title" | "deadline">;
+
+export const VowPreview = ({ title, deadline }: VowPreviewProps) => {
+  return <div></div>;
+};
