25f8b555b7050d68c68f672facbc93ca65586d78
Delta6626 <hassan911ee@gmail.com>
2026-03-25 21:13:38 +0400

    Update VowManager.tsx

--- components/dashboard/VowManager.tsx
+++ components/dashboard/VowManager.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { VowManagerTabs } from "./VowManagerTabs";
+import { Vow } from "@/types/Vow";
 
 export const VowManager = () => {
+  const [vows, setVows] = useState<Vow[]>();
+
+  useEffect(() => {
+    const fetchVows = async () => {
+      try {
+        const res = await fetch("/api/vows/");
+        const data = await res.json();
+        setVows(data.vows as Vow[]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchVows();
+  }, []);
+
   return (
     <div>
       <VowManagerTabs />
+      <div className="mt-8"></div>
     </div>
   );
 };
