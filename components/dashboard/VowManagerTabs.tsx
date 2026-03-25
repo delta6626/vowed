db82decd36bc14023a2ae70edef2faf967d9e034
Delta6626 <hassan911ee@gmail.com>
2026-03-25 20:22:56 +0400

    add logic + style

--- components/dashboard/VowManagerTabs.tsx
+++ components/dashboard/VowManagerTabs.tsx
@@ -1,13 +1,29 @@
 "use client";
 
+import { useVowManagerTab } from "@/hooks/useVowManagerTab";
+
 export type VowManagerTab = "active" | "moment-of-truth" | "resolved";
 
 export const VowManagerTabs = () => {
+  const { activeTab, setActiveTab } = useVowManagerTab();
+
   return (
-    <div className="bg-base-200 rounded-xl border border-base-100 w-fit p-2">
-      <button className="btn">Active</button>
-      <button className="btn">Moment of truth</button>
-      <button className="btn">Resolved</button>
+    <div className="bg-base-200 rounded-xl border border-base-300 w-fit p-2 flex gap-2">
+      <button
+        className={`btn ${activeTab === "active" ? "btn-primary" : "text-accent"}`}
+      >
+        Active
+      </button>
+      <button
+        className={`btn ${activeTab === "moment-of-truth" ? "btn-primary" : "text-accent"}`}
+      >
+        Moment of truth
+      </button>
+      <button
+        className={`btn ${activeTab === "resolved" ? "btn-primary" : "text-accent"}`}
+      >
+        Resolved
+      </button>
     </div>
   );
 };
