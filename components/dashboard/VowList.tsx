f63fbbf176c4da801e1770ce99da2b9e085cd871
Delta6626 <hassan911ee@gmail.com>
2026-03-25 22:05:07 +0400

    Update VowList.tsx

--- components/dashboard/VowList.tsx
+++ components/dashboard/VowList.tsx
@@ -18,22 +18,77 @@ export const VowList = ({ vows }: VowListProps) => {
     (vow) => vow.status === "fulfilled" || vow.status == "not-fulfilled",
   );
 
-  console.log(activeVows);
-
   if (activeTab === "active") {
     return (
       <div>
-        {activeVows.map((vow) => {
-          return (
-            <BasicVowContainer
-              key={vow.vowId}
-              vowText={vow.title}
-              vowStatus={vow.status}
-              vowViewCount={vow.viewCount}
-              vowCommentCount={vow.commentCount}
-            />
-          );
-        })}
+        {activeVows.length != 0 &&
+          activeVows.map((vow) => {
+            return (
+              <BasicVowContainer
+                key={vow.vowId}
+                vowText={vow.title}
+                vowStatus={vow.status}
+                vowViewCount={vow.viewCount}
+                vowCommentCount={vow.commentCount}
+              />
+            );
+          })}
+
+        {activeVows.length === 0 && (
+          <p className="w-full text-accent text-center">
+            No vows to show here.
+          </p>
+        )}
+      </div>
+    );
+  }
+
+  if (activeTab === "moment-of-truth") {
+    return (
+      <div>
+        {momentOfTruthVows.length != 0 &&
+          momentOfTruthVows.map((vow) => {
+            return (
+              <BasicVowContainer
+                key={vow.vowId}
+                vowText={vow.title}
+                vowStatus={vow.status}
+                vowViewCount={vow.viewCount}
+                vowCommentCount={vow.commentCount}
+              />
+            );
+          })}
+
+        {momentOfTruthVows.length === 0 && (
+          <p className="w-full text-accent text-center">
+            No vows to show here.
+          </p>
+        )}
+      </div>
+    );
+  }
+
+  if (activeTab === "resolved") {
+    return (
+      <div>
+        {resolvedVows.length != 0 &&
+          resolvedVows.map((vow) => {
+            return (
+              <BasicVowContainer
+                key={vow.vowId}
+                vowText={vow.title}
+                vowStatus={vow.status}
+                vowViewCount={vow.viewCount}
+                vowCommentCount={vow.commentCount}
+              />
+            );
+          })}
+
+        {resolvedVows.length === 0 && (
+          <p className="w-full text-accent text-center">
+            No vows to show here.
+          </p>
+        )}
       </div>
     );
   }
