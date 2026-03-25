2f78b3cb71f60b7b733c0015474febee23c55df8
Delta6626 <hassan911ee@gmail.com>
2026-03-25 20:24:59 +0400

    onClick handler

--- components/dashboard/VowManagerTabs.tsx
+++ components/dashboard/VowManagerTabs.tsx
@@ -10,16 +10,25 @@ export const VowManagerTabs = () => {
   return (
     <div className="bg-base-200 rounded-xl border border-base-300 w-fit p-2 flex gap-2">
       <button
+        onClick={() => {
+          setActiveTab("active");
+        }}
         className={`btn ${activeTab === "active" ? "btn-primary" : "text-accent"}`}
       >
         Active
       </button>
       <button
+        onClick={() => {
+          setActiveTab("moment-of-truth");
+        }}
         className={`btn ${activeTab === "moment-of-truth" ? "btn-primary" : "text-accent"}`}
       >
         Moment of truth
       </button>
       <button
+        onClick={() => {
+          setActiveTab("resolved");
+        }}
         className={`btn ${activeTab === "resolved" ? "btn-primary" : "text-accent"}`}
       >
         Resolved
