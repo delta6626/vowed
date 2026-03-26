1a86b6d050286905be22c7ccf95d68ca78b8da2a
Delta6626 <hassan911ee@gmail.com>
2026-03-26 21:49:13 +0400

    Create VowManagerLoading.tsx

---
+++ components/dashboard/VowManagerLoading.tsx
@@ -0,0 +1,10 @@
+export const VowManagerLoading = () => {
+  return (
+    <>
+      <div className="w-lg min-h-20 rounded-xl skeleton mb-8"></div>
+      <div className="w-full min-h-30 rounded-xl skeleton"></div>
+      <div className="w-full min-h-30 rounded-xl skeleton"></div>
+      <div className="w-full min-h-30 rounded-xl skeleton"></div>
+    </>
+  );
+};
