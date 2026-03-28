70797519201e3787136143562096c0b1db49e5ed
Delta6626 <hassan911ee@gmail.com>
2026-03-29 01:12:16 +0400

    Update SideSectionContent.tsx

--- components/auth/SideSectionContent.tsx
+++ components/auth/SideSectionContent.tsx
@@ -17,16 +17,22 @@ export default function SideSectionContent() {
           vowText={"Ship our MVP before the Series A"}
           vowStatus={"waiting"}
           className="w-xl bg-base-200/60"
+          vowViewCount={12000}
+          vowCommentCount={120}
         />
         <BasicVowContainer
           vowText={"Run a marathon before turning 30"}
           vowStatus={"fulfilled"}
           className="w-xl bg-base-200/60"
+          vowViewCount={9000}
+          vowCommentCount={70}
         />
         <BasicVowContainer
           vowText={"Finish my essay before its due"}
           vowStatus={"not-fulfilled"}
           className="w-xl bg-base-200/60"
+          vowViewCount={1200}
+          vowCommentCount={40}
         />
       </div>
     </div>
