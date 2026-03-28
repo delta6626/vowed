14418630cd528640685495b23922210181f07816
Delta6626 <hassan911ee@gmail.com>
2026-03-29 00:34:39 +0400

    Create VowResolver.tsx

---
+++ components/dashboard/VowResolver.tsx
@@ -0,0 +1,9 @@
+import { ArrowRight } from "lucide-react";
+
+export const VowResolver = () => {
+  return (
+    <button className="btn btn-secondary">
+      Resolve <ArrowRight size={20} />
+    </button>
+  );
+};
