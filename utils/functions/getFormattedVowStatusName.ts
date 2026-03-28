c92cab5b8cdd722b17ed2a7fba5cf9737cf1558a
Delta6626 <hassan911ee@gmail.com>
2026-03-29 01:03:45 +0400

    Update getFormattedVowStatusName.ts

--- utils/functions/getFormattedVowStatusName.ts
+++ utils/functions/getFormattedVowStatusName.ts
@@ -1,14 +1,17 @@
 import { VowStatus } from "@/types/VowStatus";
 
-export const getFormattedVowStatusName = (vowStatus: VowStatus) => {
+export const getFormattedVowStatusName = (
+  vowStatus: VowStatus,
+  fallback: string,
+) => {
   switch (vowStatus) {
     case "waiting":
       return "Waiting";
-    case "moment-of-truth":
-      return "Moment of Truth";
     case "fulfilled":
       return "Fulfilled";
     case "not-fulfilled":
       return "Not Fulfilled";
   }
+
+  return fallback;
 };
