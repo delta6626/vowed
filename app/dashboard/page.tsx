14355166ab3ba5400e48558ab77f3b84547a54ef
Delta6626 <hassan911ee@gmail.com>
2026-03-31 21:02:31 +0400

    Major refactor 5

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { QUERY_KEYS } from "@/constants/QUERY_KEYS";
 import { User } from "@/types/User";
 import { getGreeting } from "@/utils/functions/getGreeting";
 import { useQuery } from "@tanstack/react-query";
-import { Plus } from "lucide-react";
+import { AlertCircle, Plus } from "lucide-react";
 import Link from "next/link";
 import { Suspense } from "react";
 
@@ -15,17 +15,51 @@ export default function Dashboard() {
   const {
     data: user,
     isLoading: userLoading,
-    isError,
+    isError: userLoadingError,
   } = useQuery({
     queryKey: [QUERY_KEYS.USER_PROFILE],
     queryFn: async () => {
-      const res = await fetch("/api/user");
+      const res = await fetch("/api/user1");
       if (!res.ok) {
         throw new Error("Failed to fetch user.");
       }
       return res.json() as Promise<User>;
     },
+    retry: 3,
   });
+
+  if (userLoading) {
+    return (
+      <>
+        <Navbar />
+        <div className="doublePaddingContainer py-16">
+          <div className="skeleton w-full h-20 rounded-xl"></div>
+          <DashboardStatsLoading />
+        </div>
+      </>
+    );
+  }
+
+  if (userLoadingError) {
+    return (
+      <div className="w-screen h-screen flex flex-col">
+        <Navbar />
+
+        <div className="flex flex-col flex-1 font-body items-center justify-center paddingContainer w-screen">
+          <p className="flex items-center gap-2 text-xl">
+            <AlertCircle size={20} className="text-error" /> Something went
+            wrong.
+          </p>
+
+          <p className="text-accent text-center">
+            An error occured while trying to load your profile.
+            <br></br>
+            {userLoadingError}
+          </p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="w-screen h-screen">
