9b5f3b2df5b9feea2fc14f3df8d38989b6bee81e
Delta6626 <hassan911ee@gmail.com>
2026-03-31 18:29:12 +0400

    Major refactor - 1

--- components/dashboard/DashboardStats.tsx
+++ components/dashboard/DashboardStats.tsx
@@ -1,40 +1,14 @@
-import {
-  getActiveVowCount,
-  getFulfillmentRate,
-  getTotalVows,
-} from "@/utils/data/vows";
 import { auth } from "@clerk/nextjs/server";
 import DashboardStatBox from "../generic/DashboardStatBox";
 
 export const DashboardStats = async () => {
   const { userId } = await auth();
 
-  let activeVowCount = 0;
-  let fulfillmentRate = 0;
-  let totalVows = 0;
-
-  try {
-    const [activeCount, rate, total] = await Promise.all([
-      getActiveVowCount(userId!),
-      getFulfillmentRate(userId!),
-      getTotalVows(userId!),
-    ]);
-
-    activeVowCount = activeCount;
-    fulfillmentRate = rate;
-    totalVows = total;
-  } catch (error) {
-    console.error("Failed to fetch dashboard stats:", error);
-  }
-
   return (
     <div className="flex gap-4 mt-8">
-      <DashboardStatBox statBoxType={"active"} mainText={activeVowCount} />
-      <DashboardStatBox
-        statBoxType={"fulfillment"}
-        mainText={`${fulfillmentRate}%`}
-      />
-      <DashboardStatBox statBoxType={"total"} mainText={totalVows} />
+      <DashboardStatBox statBoxType={"active"} mainText={0} />
+      <DashboardStatBox statBoxType={"fulfillment"} mainText={`${0}%`} />
+      <DashboardStatBox statBoxType={"total"} mainText={0} />
     </div>
   );
 };
