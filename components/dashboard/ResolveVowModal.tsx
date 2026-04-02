33ec2b5d36635c031481cfb037190be234e2cdfc
Delta6626 <hassan911ee@gmail.com>
2026-04-02 15:09:53 +0400

    new modal

---
+++ components/dashboard/ResolveVowModal.tsx
@@ -0,0 +1,9 @@
+import { MODALS } from "@/constants/MODALS";
+
+export const ResolveVowModal = () => {
+  return (
+    <dialog id={MODALS.RESOLVE_VOW_MODAL.ID} className="modal">
+      <div className="modal-box"></div>
+    </dialog>
+  );
+};
---
+++ constants/MODALS.ts
@@ -0,0 +1,6 @@
+export const MODALS = {
+  RESOLVE_VOW_MODAL: {
+    ID: "resolveVowModal",
+    TITLE: "Resolve this vow",
+  },
+};
