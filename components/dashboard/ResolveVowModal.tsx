51c83ef0315a17a75497a90b18b24ad7ff376557
Delta6626 <hassan911ee@gmail.com>
2026-04-03 16:07:35 +0400

    add title

--- components/dashboard/ResolveVowModal.tsx
+++ components/dashboard/ResolveVowModal.tsx
@@ -3,7 +3,11 @@ import { MODALS } from "@/constants/MODALS";
 export const ResolveVowModal = () => {
   return (
     <dialog id={MODALS.RESOLVE_VOW_MODAL.ID} className="modal">
-      <div className="modal-box"></div>
+      <div className="modal-box border border-base-300">
+        <h1 className="text-lg text-base-content/80">
+          {MODALS.RESOLVE_VOW_MODAL.TITLE}
+        </h1>
+      </div>
     </dialog>
   );
 };
