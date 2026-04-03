8c8252cacca36c388100be912f571dfa42061e4f
Delta6626 <hassan911ee@gmail.com>
2026-04-03 16:38:19 +0400

    add buttons

--- components/dashboard/ResolveVowModal.tsx
+++ components/dashboard/ResolveVowModal.tsx
@@ -1,12 +1,43 @@
 import { MODALS } from "@/constants/MODALS";
+import { useSelectedVowStore } from "@/store/selectedVowStore";
+import { VowResolution } from "@/types/VowResolution";
+import { Check, X } from "lucide-react";
+import { useState } from "react";
 
 export const ResolveVowModal = () => {
+  const { vowDetails } = useSelectedVowStore();
+  const [vowResolution, setVowResolution] =
+    useState<Pick<VowResolution, "outcome" | "resolutionNote">>();
+
   return (
     <dialog id={MODALS.RESOLVE_VOW_MODAL.ID} className="modal">
       <div className="modal-box border border-base-300">
-        <h1 className="text-lg text-base-content/80">
-          {MODALS.RESOLVE_VOW_MODAL.TITLE}
-        </h1>
+        <div className="">
+          <h1 className="text-xl font-display stext-base-content/80">
+            {MODALS.RESOLVE_VOW_MODAL.TITLE}
+          </h1>
+          <p className="text-accent italic mt-2">{`"${vowDetails.vowTitle}"`}</p>
+        </div>
+
+        <div className="mt-4 flex flex-col gap-2">
+          <button className="bg-base-200 rounded-xl w-full flex gap-4 items-center justify-start p-4 border border-base-300 hover:border-success/20">
+            <Check className="text-success" />
+            <div className="flex flex-col items-start">
+              <h1>Fulfilled</h1>
+              <p className="text-sm text-accent">
+                This event happened as vowed.
+              </p>
+            </div>
+          </button>
+
+          <button className="bg-base-200 rounded-xl w-full flex gap-4 items-center justify-start p-4 border border-base-300 hover:border-error/20">
+            <X className="text-error" />
+            <div className="flex flex-col items-start">
+              <h1>Not fulfilled</h1>
+              <p className="text-sm text-accent">This event did not happen.</p>
+            </div>
+          </button>
+        </div>
       </div>
     </dialog>
   );
