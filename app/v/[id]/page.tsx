b1d0f3c8cbcb3ff5273b7c8314b69df60482c9aa
Delta6626 <hassan911ee@gmail.com>
2026-04-06 15:33:40 +0400

    trigger not found

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -1,3 +1,7 @@
+import Navbar from "@/components/navigation/Navbar";
+import { adminDb } from "@/utils/firebase/admin";
+import { notFound } from "next/navigation";
+
 interface VowPageProps {
   params: {
     id: string;
@@ -6,9 +10,17 @@ interface VowPageProps {
 
 export default async function Vow({ params }: VowPageProps) {
   const { id } = await params;
+  const vowSnaphsot = await adminDb.collection("vows").doc(id).get();
+
+  if (!vowSnaphsot.exists) {
+    notFound();
+  }
+
   return (
-    <>
-      <h1>{id}</h1>
-    </>
+    <div className="flex flex-col w-screen h-screen">
+      <div className="flex flex-col flex-1">
+        <Navbar />
+      </div>
+    </div>
   );
 }
