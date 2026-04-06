cb6172d351e27e88d42ab1ba863ceb0b225bdc06
Delta6626 <hassan911ee@gmail.com>
2026-04-06 16:34:23 +0400

    fetch data

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/navigation/Navbar";
+import { User } from "@/types/User";
+import type { Vow } from "@/types/Vow";
 import { adminDb } from "@/utils/firebase/admin";
 import { notFound } from "next/navigation";
 
@@ -16,10 +18,21 @@ export default async function Vow({ params }: VowPageProps) {
     notFound();
   }
 
+  const vowData = vowSnaphsot.data() as Vow;
+  const associatedUserSnapshsot = await adminDb
+    .collection("users")
+    .doc(vowData.authorId)
+    .get();
+  const associatedUserData = associatedUserSnapshsot.data() as User;
+
   return (
     <div className="flex flex-col w-screen h-screen">
       <div className="flex flex-col flex-1">
         <Navbar />
+      </div>
+
+      <div className="w-full h-full doublePaddingContainer mt-16">
+        <h1 className="font-display text-4xl">{vowData.title}</h1>
       </div>
     </div>
   );
