6b76852dfe622d701d40383806dc774d0df79e70
Delta6626 <hassan911ee@gmail.com>
2026-04-07 16:41:19 +0400

    create types

---
+++ types/GetVowRespsonse.ts
@@ -0,0 +1,23 @@
+import { VowComment } from "./VowComment";
+import { VowResolution } from "./VowResolution";
+import { VowStatus } from "./VowStatus";
+
+export interface GetVowResponse {
+  vowId: string;
+
+  currentTimestampUTC: number;
+  vowStatus: VowStatus;
+  vowStatusFormatted: string;
+  vowCreationDate: Date;
+  vowTitle: string;
+  vowDescription: string;
+  vowDeadlineTimestampUTC: number;
+  vowCommentCount: number;
+  vowViewCount: number;
+  vowComments: VowComment[];
+  vowResolution: VowResolution | null;
+
+  vowCreatorProfilePhoto: string;
+  vowCreatorName: string;
+  vowCreatorFulfillmentRate: number;
+}
---
+++ types/VowComment.ts
@@ -0,0 +1,6 @@
+export interface VowComment {
+  commentId?: string;
+  commenterName: string;
+  commentText: string;
+  commentDate: Date;
+}
