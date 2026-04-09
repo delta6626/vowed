7234658f9b9abf03e213be5965b7d85dd029a78e
Delta6626 <hassan911ee@gmail.com>
2026-04-09 14:13:51 +0400

    sync time

--- components/generic/VowWaiting.tsx
+++ components/generic/VowWaiting.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export interface VowWaitingProps {
   initialCurrentTimestampUTC: number;
   deadlineTimestampUTC: number;
@@ -7,5 +9,22 @@ export const VowWaiting = ({
   initialCurrentTimestampUTC,
   deadlineTimestampUTC,
 }: VowWaitingProps) => {
-  return <div className=""></div>;
+  const [currentTimestampUTC, setCurrentTimestampUTC] = useState<number>();
+
+  useEffect(() => {
+    if (initialCurrentTimestampUTC !== undefined) {
+      setCurrentTimestampUTC(initialCurrentTimestampUTC);
+    }
+  }, [initialCurrentTimestampUTC]);
+
+  useEffect(() => {
+    if (!currentTimestampUTC) return;
+    const timeSyncInterval = setInterval(() => {
+      setCurrentTimestampUTC((prev) => prev! + 1000);
+    }, 1000);
+
+    return () => clearInterval(timeSyncInterval);
+  }, [currentTimestampUTC === undefined]);
+
+  return <div className="w-full h-20 border border-base-300 rounded-xl"></div>;
 };
