eb1634b274bfa7c34298fec9d99aab44704409d7
Delta6626 <hassan911ee@gmail.com>
2026-04-09 15:05:58 +0400

    add countdown

--- components/generic/VowWaiting.tsx
+++ components/generic/VowWaiting.tsx
@@ -1,3 +1,5 @@
+import { getCountdown } from "@/utils/functions/getCountdown";
+import { Dot } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export interface VowWaitingProps {
@@ -10,6 +12,10 @@ export const VowWaiting = ({
   deadlineTimestampUTC,
 }: VowWaitingProps) => {
   const [currentTimestampUTC, setCurrentTimestampUTC] = useState<number>();
+
+  const countdown = currentTimestampUTC
+    ? getCountdown(deadlineTimestampUTC, currentTimestampUTC)
+    : undefined;
 
   useEffect(() => {
     if (initialCurrentTimestampUTC !== undefined) {
@@ -26,5 +32,58 @@ export const VowWaiting = ({
     return () => clearInterval(timeSyncInterval);
   }, [currentTimestampUTC === undefined]);
 
-  return <div className="w-full h-20 border border-base-300 rounded-xl"></div>;
+  if (countdown === undefined) {
+    return <div></div>;
+  }
+
+  if (countdown === "DEADLINE_PASSED") {
+    return (
+      <div className="w-full h-20 border border-base-300 rounded-2xl"></div>
+    );
+  }
+
+  return (
+    <div className="bg-base-200 w-full p-8 border border-base-300 rounded-2xl">
+      <h1 className="text-center uppercase font-medium text-accent">
+        Time remaining
+      </h1>
+
+      <p className="mt-4 flex items-center gap-4 w-full justify-center text-5xl font-mono tabular-nums">
+        {countdown.years !== 0 && (
+          <>
+            <span>{countdown.years}y</span>
+            <Dot />
+          </>
+        )}
+
+        {countdown.days !== 0 && (
+          <>
+            <span>{countdown.days}d</span>
+            <Dot />
+          </>
+        )}
+
+        <span>{countdown.hours}h</span>
+        <Dot />
+
+        <span>{countdown.minutes}m</span>
+        <Dot />
+
+        <span>{countdown.seconds}s</span>
+      </p>
+
+      <p className="mt-4 text-base-content/80 text-center font-medium">
+        <span className="text-accent">{"Due "}</span>
+        {`${new Date(deadlineTimestampUTC).toLocaleString("en-US", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "numeric",
+          minute: "2-digit",
+          hour12: true,
+        })}`}
+      </p>
+    </div>
+  );
 };
