b487f42afc1f998788e40f43123b5796364e27fe
Delta6626 <hassan911ee@gmail.com>
2026-04-09 15:20:59 +0400

    add gradient

--- components/generic/VowWaiting.tsx
+++ components/generic/VowWaiting.tsx
@@ -43,7 +43,7 @@ export const VowWaiting = ({
   }
 
   return (
-    <div className="bg-base-200 w-full p-8 border border-base-300 rounded-2xl">
+    <div className="bg-base-200 w-full p-8 border border-base-300 rounded-2xl bg-radial-[at_50%_75%] from-primary/40 to-base-200 to-90%">
       <h1 className="text-center uppercase font-medium text-accent">
         Time remaining
       </h1>
