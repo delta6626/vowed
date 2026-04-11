efcb9a9d93901259367c6856acbb4521eaa360b6
Delta6626 <hassan911ee@gmail.com>
2026-04-11 15:03:55 +0400

    add resolution timestamp

--- components/generic/VowResolved.tsx
+++ components/generic/VowResolved.tsx
@@ -10,6 +10,20 @@ export const VowResolved = ({
   return (
     <div className="bg-base-200 w-full p-8 border border-success/10 rounded-2xl bg-radial-[at_50%_75%] from-success/50 via-success/20 to-success/10 to-90%">
       <h1 className="text-4xl font-display text-center">Vow fulfilled</h1>
+      <p className="mt-4 text-base-content/80 text-center">
+        {`Resolved by creator on ${new Date(resolutionTimestamp).toLocaleString(
+          "en-US",
+          {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: true,
+          },
+        )}`}
+      </p>
     </div>
   );
 };
