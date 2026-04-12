0c908124337a82c86706fc639449840805e8e0c2
Delta6626 <hassan911ee@gmail.com>
2026-04-12 12:48:51 +0400

    create form for comments

---
+++ components/generic/VowCommentForm.tsx
@@ -0,0 +1,29 @@
+import { SubmitEvent } from "react";
+
+export const VowCommentForm = () => {
+  const handleSubmitComment = (e: SubmitEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <h1>Comments</h1>
+
+      <form onSubmit={handleSubmitComment}>
+        <textarea
+          className="mt-4 textarea resize-none w-full p-4 bg-base-200 rounded-xl border border-base-300 min-h-25 max-h-25"
+          placeholder={"Leave a comment"}
+        />
+
+        <div className="mt-2 flex items-center justify-between">
+          <p className="text-accent">
+            Comments must follow our community guidelines. Please be respectful.
+          </p>
+          <button type={"submit"} className="btn btn-primary">
+            Post comment
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
