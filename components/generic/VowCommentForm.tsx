ac760b460e1d7caad13cda57420ad7ec40c2ccf2
Delta6626 <hassan911ee@gmail.com>
2026-04-12 12:53:00 +0400

    add state

--- components/generic/VowCommentForm.tsx
+++ components/generic/VowCommentForm.tsx
@@ -1,6 +1,12 @@
-import { SubmitEvent } from "react";
+import { ChangeEvent, SubmitEvent, useState } from "react";
 
 export const VowCommentForm = () => {
+  const [comment, setComment] = useState<string>("");
+
+  const handleCommentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  };
+
   const handleSubmitComment = (e: SubmitEvent) => {
     e.preventDefault();
   };
@@ -11,6 +17,9 @@ export const VowCommentForm = () => {
 
       <form onSubmit={handleSubmitComment}>
         <textarea
+          value={comment}
+          onChange={handleCommentChange}
+          maxLength={300}
           className="mt-4 textarea resize-none w-full p-4 bg-base-200 rounded-xl border border-base-300 min-h-25 max-h-25"
           placeholder={"Leave a comment"}
         />
@@ -19,7 +28,11 @@ export const VowCommentForm = () => {
           <p className="text-accent">
             Comments must follow our community guidelines. Please be respectful.
           </p>
-          <button type={"submit"} className="btn btn-primary">
+          <button
+            type={"submit"}
+            className="btn btn-primary"
+            disabled={!comment}
+          >
             Post comment
           </button>
         </div>
