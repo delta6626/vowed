e4d9e5c9143040bb8dfd0267cd2e6ee996187ab5
Delta6626 <hassan911ee@gmail.com>
2026-04-13 13:41:53 +0400

    add auth check

--- app/api/vows/[id]/comment/route.ts
+++ app/api/vows/[id]/comment/route.ts
@@ -1,9 +1,16 @@
 import { auth } from "@clerk/nextjs/server";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> },
 ) {
   const [{ id: vowId }, { userId }] = await Promise.all([params, auth()]);
+
+  if (!userId) {
+    return NextResponse.json(
+      { error: "User must be authenticated to leave a comment." },
+      { status: 401 },
+    );
+  }
 }
