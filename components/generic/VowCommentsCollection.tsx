89d35db406343f225c1072e92f8d3fe251a37b90
Delta6626 <hassan911ee@gmail.com>
2026-04-13 13:33:31 +0400

    type + layout + render

--- components/generic/VowCommentsCollection.tsx
+++ components/generic/VowCommentsCollection.tsx
@@ -1,3 +1,18 @@
-export const VowCommentsCollection = () => {
-  return <div></div>;
+import { VowComment } from "@/types/VowComment";
+import { VowCommentContainer } from "./VowCommentContainer";
+
+export interface VowCommentsCollectionProps {
+  commentsCollection: VowComment[];
+}
+
+export const VowCommentsCollection = ({
+  commentsCollection,
+}: VowCommentsCollectionProps) => {
+  return (
+    <>
+      {commentsCollection.map((comment) => {
+        return <VowCommentContainer comment={comment} />;
+      })}
+    </>
+  );
 };
