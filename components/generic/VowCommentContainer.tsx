aeb21d85eb13fcde7398b290456d527f7b6dbed2
Delta6626 <hassan911ee@gmail.com>
2026-04-13 13:33:22 +0400

    define types + basic layout

---
+++ components/generic/VowCommentContainer.tsx
@@ -0,0 +1,9 @@
+import { VowComment } from "@/types/VowComment";
+
+export interface VowCommentContainerProps {
+  comment: VowComment;
+}
+
+export const VowCommentContainer = ({ comment }: VowCommentContainerProps) => {
+  return <div className=""></div>;
+};
