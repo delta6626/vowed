1950bebe50381f86a0599bd2f0405a4a4df522e6
Delta6626 <hassan911ee@gmail.com>
2026-04-14 23:31:21 +0400

    add content

--- components/generic/VowCommentContainer.tsx
+++ components/generic/VowCommentContainer.tsx
@@ -1,9 +1,31 @@
 import { VowComment } from "@/types/VowComment";
+import Link from "next/link";
 
 export interface VowCommentContainerProps {
   comment: VowComment;
 }
 
 export const VowCommentContainer = ({ comment }: VowCommentContainerProps) => {
-  return <div className="">{comment.commentText}</div>;
+  return (
+    <div className="bg-base-200 p-4 rounded-xl">
+      <div className="flex items-center w-full justify-between">
+        <Link
+          href={`u/${comment.commenterId}`}
+          className="text-base-content/80 font-medium"
+        >
+          {comment.commenterName}
+        </Link>
+
+        <p className="text-accent">
+          {new Date(comment.commentCreationTimestamp).toLocaleString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </p>
+      </div>
+
+      <p className="mt-2">{comment.commentText}</p>
+    </div>
+  );
 };
