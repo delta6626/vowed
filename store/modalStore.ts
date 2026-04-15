0938fa54bd976204047e21e8c1ef505b18dd6731
Delta6626 <hassan911ee@gmail.com>
2026-04-15 12:10:57 +0400

    create new store

--- store/modalStore.ts
+++ store/modalStore.ts
@@ -2,10 +2,21 @@ import { create } from "zustand";
 
 export type ModalType = "error" | "success" | "alert";
 
-export interface ModalStore {
+export interface ModalInformation {
   modalType: ModalType;
   modalTitle: string;
   modalText: string;
+  onPrimaryButtonClick?: () => void;
+  onSecondaryButtonClick?: () => void;
 }
 
-export const useModalStore = create(() => {});
+export interface ModalStore {
+  modalInformation: ModalInformation | null;
+  setModalInformation: (updatedModalInformation: ModalInformation) => void;
+}
+
+export const useModalStore = create<ModalStore>((set) => ({
+  modalInformation: null,
+  setModalInformation: (updatedModalInformation) =>
+    set({ modalInformation: updatedModalInformation }),
+}));
