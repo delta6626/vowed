a7e50ea9db1e9f40b934f08b0c14f40a881263bc
Delta6626 <hassan911ee@gmail.com>
2026-04-16 14:00:39 +0400

    create base layout

--- app/u/[id]/page.tsx
+++ app/u/[id]/page.tsx
@@ -1,3 +1,13 @@
+import Navbar from "@/components/navigation/Navbar";
+
 export const UserProfile = () => {
-  return <></>;
+  return (
+    <div className="flex flex-col w-screen h-screen overflow-x-hidden">
+      <div className="flex flex-col flex-1">
+        <Navbar />
+      </div>
+
+      <div className="w-full h-full doublePaddingContainer mt-16"></div>
+    </div>
+  );
 };
