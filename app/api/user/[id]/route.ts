f2d0e493689d57eb4abf118ddaa47f6f95386a33
Delta6626 <hassan911ee@gmail.com>
2026-04-16 15:11:43 +0400

    auth check

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,6 +1,19 @@
-import { NextRequest } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> },
-) {}
+) {
+  const [{ id: requestedUserId }, { userId }] = await Promise.all([
+    params,
+    auth(),
+  ]);
+
+  if (!userId) {
+    return NextResponse.json(
+      { error: "User must be authenticated" },
+      { status: 401 },
+    );
+  }
+}
