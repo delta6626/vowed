490ad3d0921179ff54060b5c97bc3d4d3d90f7b2
Delta6626 <hassan911ee@gmail.com>
2026-04-19 15:29:24 +0400

    add ui

--- components/generic/PublicVowContainer.tsx
+++ components/generic/PublicVowContainer.tsx
@@ -1,4 +1,6 @@
 import { PublicVowResponse } from "@/types/PublicVowResponse";
+import { Eye, MessageCircle } from "lucide-react";
+import Link from "next/link";
 
 export const PublicVowContainer = ({
   vowId,
@@ -8,5 +10,24 @@ export const PublicVowContainer = ({
   viewCount,
   createdAt,
 }: PublicVowResponse) => {
-  return <div></div>;
+  return (
+    <div className="flex justify-between font-body px-8 py-4 rounded-2xl border border-base-300 bg-base-200">
+      <div className="flex flex-col gap-4 justify-between">
+        <Link href={`/v/${vowId}`} className="text-base-content/70">
+          {title}
+        </Link>
+        <div className="text-base flex gap-4 items-center text-accent">
+          <div className="flex gap-2 items-center">
+            <Eye size={20} />
+            <p>{viewCount ?? 0}</p>
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <MessageCircle size={20} />
+            <p>{commentCount ?? 0}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
