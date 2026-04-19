f544beaa1de9867567593b5546be13fa23f78ddc
Delta6626 <hassan911ee@gmail.com>
2026-04-19 15:24:34 +0400

    create component

---
+++ components/generic/PublicVowContainer.tsx
@@ -0,0 +1,12 @@
+import { PublicVowResponse } from "@/types/PublicVowResponse";
+
+export const PublicVowContainer = ({
+  vowId,
+  statusFormatted,
+  title,
+  commentCount,
+  viewCount,
+  createdAt,
+}: PublicVowResponse) => {
+  return <div></div>;
+};
