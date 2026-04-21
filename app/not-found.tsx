eda395adf491d7e60c92ded66fcda126371563ad
Delta6626 <hassan911ee@gmail.com>
2026-04-21 14:35:55 +0400

    logic

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/navigation/Navbar";
+import { Show } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function NotFound() {
@@ -18,9 +19,17 @@ export default function NotFound() {
           You've wandered somewhere we haven't built yet.
         </p>
 
-        <Link href={"/"} className="mt-8 btn btn-primary">
-          Go to homepage
-        </Link>
+        <Show when={"signed-out"}>
+          <Link href={"/"} className="mt-8 btn btn-primary">
+            Go Home
+          </Link>
+        </Show>
+
+        <Show when={"signed-in"}>
+          <Link href={"/"} className="mt-8 btn btn-primary">
+            Go to Dashboard
+          </Link>
+        </Show>
       </div>
     </div>
   );
