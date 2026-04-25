06c026b1c14105da59c9eaca3eecb5e5b3cccecf
Delta6626 <hassan911ee@gmail.com>
2026-04-25 14:50:34 +0400

    reduce gap

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mt-16 flex gap-8 items-start justify-center">
+        <div className="mt-16 flex gap-4 items-start justify-center">
           <div className="p-4 border border-base-300 max-w-sm rounded-xl bg-base-200">
             <Pill
               className="w-fit"
