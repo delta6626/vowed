02a7ee9709f6ae6f54b88facc7a1bb83ee9523a8
Delta6626 <hassan911ee@gmail.com>
2026-04-25 13:16:18 +0400

    change Pill

--- components/generic/Pill.tsx
+++ components/generic/Pill.tsx
@@ -1,6 +1,11 @@
 import { ReactNode } from "react";
 
-export type PillVariant = "primary" | "secondary" | "success" | "error";
+export type PillVariant =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "error"
+  | "generic";
 
 export interface PillProps {
   text: string;
@@ -28,6 +33,10 @@ export default function Pill({ text, icon, className, variant }: PillProps) {
     case "error":
       style =
         "px-4 py-2 bg-error/20 rounded-full text-sm font-semibold text-error";
+      break;
+    case "generic":
+      style =
+        "uppercase px-4 py-2 bg-base-200 border border-base-300 text-base-content/60 rounded-full text-sm font-semibold";
       break;
   }
 
