a5fcd0eeeb0d16c76989d0bbb847af1777213a91
Delta6626 <hassan911ee@gmail.com>
2026-04-29 17:15:44 +0400

    change style

--- app/page.tsx
+++ app/page.tsx
@@ -464,23 +464,22 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="max-w-sm">
-          <div className="flex items-center gap-2">
+        <div className="max-w-sm flex flex-col gap-4">
+          <div className="flex items-center gap-2 p-4 bg-base-200 rounded-xl">
             <Pill text="Public" variant={"success"} className="w-fit"></Pill>
             <p className="text-base-content/60">
               Anyone with or without the link. Appears on your profile.
             </p>
           </div>
 
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 p-4 bg-base-200 rounded-xl">
             <Pill text="Public" variant={"secondary"} className="w-fit"></Pill>
             <p className="text-base-content/60">
-              Anyone with the direct link. Hidden from your profile and search
-              engines.
+              Anyone with the direct link. Hidden from your profile.
             </p>
           </div>
 
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 p-4 bg-base-200 rounded-xl">
             <Pill text="Public" variant={"generic"} className="w-fit"></Pill>
             <p className="text-base-content/60">
               Only you. Full accountability with zero public pressure.
