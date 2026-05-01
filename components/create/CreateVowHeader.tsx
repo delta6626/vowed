0ee7372296623b1599ba276137f021db0bd8787b
Delta6626 <hassan911ee@gmail.com>
2026-05-01 18:03:24 +0400

    change style

--- components/create/CreateVowHeader.tsx
+++ components/create/CreateVowHeader.tsx
@@ -2,9 +2,9 @@ export const CreateVowHeader = () => {
   return (
     <div className="w-2xl">
       <h1 className="font-display text-4xl">
-        Make your <span className="text-primary">vow.</span>
+        Make your <span className="text-primary italic">vow.</span>
       </h1>
-      <p className="font-body text-accent text-xl mt-2">
+      <p className="font-body text-base-content/60 text-xl mt-2">
         Describe your vow. Set a deadline. It goes public the moment you submit.
         The world will be watching.
       </p>
