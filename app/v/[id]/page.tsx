faab1acb3304c8af7315f64027cb952d6f031c35
Delta6626 <hassan911ee@gmail.com>
2026-05-03 13:43:13 +0400

    show comment form for only vows in waiting state

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -193,7 +193,9 @@ export default function Vow() {
 
         <div className="flex flex-col gap-8 mt-8 pb-8">
           <h1 className="font-medium text-lg text-accent">Comments</h1>
-          <VowCommentForm vowId={vowResponse.vowId} />
+          {vowStatus === "waiting" && (
+            <VowCommentForm vowId={vowResponse.vowId} />
+          )}
           <VowCommentsCollection commentsCollection={vowResponse.vowComments} />
         </div>
       </div>
