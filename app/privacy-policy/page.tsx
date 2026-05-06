de9ca067011b6c9b4409365de6d48cc348449c30
Delta6626 <hassan911ee@gmail.com>
2026-05-07 02:09:36 +0400

    style

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -7,7 +7,33 @@ export default function PrivacyPolicy() {
     <div className="w-screen h-screen overflow-x-hidden bg-base-100">
       <Navbar />
       <div className="mt-16 doublePaddingContainer">
-        <Markdown>{PRIVACY_POLICY_CONTENT_MARKDOWN}</Markdown>
+        <Markdown
+          components={{
+            h1: ({ children }) => {
+              return <h1 className="text-3xl font-display">{children}</h1>;
+            },
+
+            h2: ({ children }) => {
+              return <h2 className="text-2xl mt-8">{children}</h2>;
+            },
+
+            h3: ({ children }) => {
+              return <h3 className="text-xl mt-4">{children}</h3>;
+            },
+
+            h4: ({ children }) => {
+              return <p className="text-base-content/60 mt-2">{children}</p>;
+            },
+
+            p: ({ children }) => {
+              return (
+                <p className="text-lg text-base-content/90 mt-2">{children}</p>
+              );
+            },
+          }}
+        >
+          {PRIVACY_POLICY_CONTENT_MARKDOWN}
+        </Markdown>
       </div>
     </div>
   );
