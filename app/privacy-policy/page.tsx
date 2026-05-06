2a7d635c1570d3123882ccc8c9a5653965c3110e
Delta6626 <hassan911ee@gmail.com>
2026-05-07 01:52:55 +0400

    add style

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,10 +1,14 @@
+import Navbar from "@/components/navigation/Navbar";
 import { PRIVACY_POLICY_CONTENT_MARKDOWN } from "@/constants/PRIVACY_POLICY";
 import Markdown from "react-markdown";
 
 export default function PrivacyPolicy() {
   return (
-    <div className="w-screen h-screen overflow-x-hidden bg-base-100 doublePaddingContainer">
-      <Markdown>{PRIVACY_POLICY_CONTENT_MARKDOWN}</Markdown>
+    <div className="w-screen h-screen overflow-x-hidden bg-base-100">
+      <Navbar />
+      <div className="mt-16 doublePaddingContainer">
+        <Markdown>{PRIVACY_POLICY_CONTENT_MARKDOWN}</Markdown>
+      </div>
     </div>
   );
 }
