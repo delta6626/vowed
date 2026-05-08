e79dac3e2d6401055377ee015227a048fb6de323
Delta6626 <hassan911ee@gmail.com>
2026-05-08 14:16:05 +0400

    create tos page

--- app/terms-of-service/page.tsx
+++ app/terms-of-service/page.tsx
@@ -1,3 +1,48 @@
+import Navbar from "@/components/navigation/Navbar";
+import { TERMS_OF_SERVICE_MARKDOWN_CONTENT } from "@/constants/TERMS_OF_SERVICE";
+import Markdown from "react-markdown";
+
 export default function TermsOfService() {
-  return <div></div>;
+  return (
+    <div className="w-screen h-screen overflow-x-hidden bg-base-100">
+      <Navbar />
+      <div className="mt-16 doublePaddingContainer">
+        <Markdown
+          components={{
+            h1: ({ children }) => {
+              return <h1 className="text-3xl font-display">{children}</h1>;
+            },
+
+            h2: ({ children }) => {
+              return <h2 className="text-2xl mt-12">{children}</h2>;
+            },
+
+            h3: ({ children }) => {
+              return <h3 className="text-xl mt-6">{children}</h3>;
+            },
+
+            h4: ({ children }) => {
+              return <p className="text-base-content/60 mt-2">{children}</p>;
+            },
+
+            p: ({ children }) => {
+              return (
+                <p className="text-lg text-base-content/90 mt-2">{children}</p>
+              );
+            },
+
+            strong: ({ children }) => {
+              return <p>{children}</p>;
+            },
+
+            li: ({ children }) => {
+              return <li className="list-disc ml-8">{children}</li>;
+            },
+          }}
+        >
+          {TERMS_OF_SERVICE_MARKDOWN_CONTENT}
+        </Markdown>
+      </div>
+    </div>
+  );
 }
