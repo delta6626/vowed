89eb5bf48e2c1adf8d0fde3dd36183ef842aa6fd
Delta6626 <hassan911ee@gmail.com>
2026-05-10 22:57:03 +0400

    update ThemeChanger component styles for consistency and adjust icon sizes

--- components/theme/ThemeChanger.tsx
+++ components/theme/ThemeChanger.tsx
@@ -14,32 +14,32 @@ export default function ThemeChanger() {
 
       <ul className="border border-base-300 dropdown-content menu bg-base-200 rounded-box w-40 mt-2">
         <button
-          className="btn flex justify-start"
+          className="btn flex justify-start text-base-content/60"
           onClick={() => {
             setTheme("light");
           }}
         >
-          <Sun size={20} />
+          <Sun size={18} />
           Light
         </button>
 
         <button
-          className="btn flex justify-start"
+          className="btn flex justify-start text-base-content/60"
           onClick={() => {
             setTheme("dark");
           }}
         >
-          <Moon size={20} />
+          <Moon size={18} />
           Dark
         </button>
 
         <button
-          className="btn flex justify-start"
+          className="btn flex justify-start text-base-content/60"
           onClick={() => {
             setTheme("system");
           }}
         >
-          <SunMoon size={20} />
+          <SunMoon size={18} />
           System
         </button>
       </ul>
