712495131ebc860e8cfd70e5b9f7ed73f5e3e56f
Delta6626 <hassan911ee@gmail.com>
2026-05-11 21:03:02 +0400

    update Dashboard component to adjust heading size for better responsiveness

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -105,7 +105,7 @@ export default function Dashboard() {
         <Navbar />
         <div className="doublePaddingContainer">
           <div className="flex flex-col mt-16">
-            <h1 className="font-display text-4xl">
+            <h1 className="font-display text-3xl sm:text-4xl">
               {getGreeting() + ", "}
               <span className="text-primary italic">
                 {user.displayName.split(" ")[0]}.
