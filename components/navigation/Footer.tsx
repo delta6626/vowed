58b84d0408466c2fc4ab16e414265c0dcfe4892f
Delta6626 <hassan911ee@gmail.com>
2026-05-14 23:10:54 +0400

    update Footer component to enhance layout structure for better responsiveness

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -5,8 +5,8 @@ import ThemeChanger from "../theme/ThemeChanger";
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-between w-full paddingContainer py-4 border-t border-base-300">
-      <div className="">
+    <footer className="flex flex-col sm:flex-row items-center justify-between w-full paddingContainer py-4 border-t border-base-300">
+      <div className="flex flex-col items-center sm:items-start mb-4 sm:mb-0">
         <Logo className="text-xl" />
         <p className="text-base-content/60 text-sm">
           Vow it. Mean it. Prove it.
