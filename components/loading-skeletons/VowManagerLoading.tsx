213db5990c82d13ee3778163ba1094b31226f9b5
Delta6626 <hassan911ee@gmail.com>
2026-05-20 23:03:12 +0400

    update VowManagerLoading component for improved responsiveness of loading skeleton

--- components/loading-skeletons/VowManagerLoading.tsx
+++ components/loading-skeletons/VowManagerLoading.tsx
@@ -1,7 +1,7 @@
 export const VowManagerLoading = () => {
   return (
     <>
-      <div className="mt-8 w-lg min-h-20 rounded-xl skeleton mb-8"></div>
+      <div className="mt-8 w-full sm:w-lg min-h-20 rounded-xl skeleton mb-8"></div>
       <div className="w-full min-h-20 rounded-xl skeleton"></div>
       <div className="w-full min-h-20 rounded-xl skeleton"></div>
       <div className="w-full min-h-20 rounded-xl skeleton"></div>
