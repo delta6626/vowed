f826012f2eceff05b4da0a69c5b4ec17b85a4984
Delta6626 <hassan911ee@gmail.com>
2026-06-20 13:48:11 +0400

    refactor layout for improved responsiveness in visibility levels section

--- app/page.tsx
+++ app/page.tsx
@@ -426,7 +426,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="mt-32 doublePaddingContainer flex items-center justify-between">
+      <div className="w-full mt-32 doublePaddingContainer flex gap-8 3xl:gap-0 flex-col 3xl:flex-row items-start 3xl:items-center justify-between">
         <div className="max-w-md">
           <p className="uppercase text-xs text-accent">Visibility levels</p>
           <h1 className="text-base-content/90 text-4xl font-display mt-4">
