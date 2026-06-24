e802c90317c6dfc43d5fc556baf9762429adffb5
Delta6626 <hassan911ee@gmail.com>
2026-06-24 16:21:06 +0400

    update sign-in and sign-up page layout for consistent height

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -6,7 +6,7 @@ export default function SignInPage() {
     <div className="w-full flex">
       <SideSection />
 
-      <div className="border-l border-base-300 w-full flex items-center bg-base-100 justify-center">
+      <div className="border-l border-base-300 w-full h-screen flex items-center bg-base-100 justify-center">
         <SignIn />
       </div>
     </div>
